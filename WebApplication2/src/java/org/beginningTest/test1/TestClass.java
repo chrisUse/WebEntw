79b73f1ff15180d59c3ae9b454fd5b6ce39643cf
Christian Linde <linde.chris@googlemail.com>
2012-10-18 13:30:11 +0200

    --

---
+++ WebApplication2/src/java/org/beginningTest/test1/TestClass.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.beginningTest.test1;
+
+/**
+ *
+ * @author christianlinde
+ */
+public class TestClass {
+  private String name;
+  
+  public TestClass() {
+    name = null;
+  }
+
+  /**
+   * @return the name
+   */
+  public String getName() {
+      return name;
+  }
+
+  /**
+   * @param name the name to set
+   */
+  public void setName(String name) {
+      this.name = name;
+  }
+
+}
