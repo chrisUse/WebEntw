59a7054f74b37512d8c6bf5ba488e9c79fca4687
Christian Linde <linde.chris@googlemail.com>
2012-10-26 15:43:46 +0200

    add example products

--- src/java/data/Product.java
+++ src/java/data/Product.java
@@ -18,5 +18,53 @@ public class Product {
     private String manufacturer;
     
     private List<Comment> comments;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
     
 }
