2e625852842d66e1629ce3663b382cf6a057d14f
Christian Linde <linde.chris@googlemail.com>
2012-10-26 17:12:01 +0200

    update storage product init

--- src/java/data/Storage.java
+++ src/java/data/Storage.java
@@ -12,63 +12,63 @@ import java.util.*;
  */
 public class Storage {
 
-	/*
-	 * should be thread safe too:
-	 * http://www.theserverside.de/singleton-pattern-in-java/
-	 */
-	private static Storage instance = new Storage();
+    /*
+     * should be thread safe too:
+     * http://www.theserverside.de/singleton-pattern-in-java/
+     */
+    private static Storage instance = new Storage();
 
-	private HashMap<Integer, Product> products;
-	private HashMap<Integer, User> users;
+    private HashMap<Integer, Product> products;
+    private HashMap<Integer, User> users;
 
-	private void addDefaultData() {
-		// add some users
-		// add some products
-		Integer tmpArtNr = 123456;
-		
-		Product myTmpStore = new Product();
-		myTmpStore.setName("Kindle Fire HD");
-		myTmpStore.setId(tmpArtNr);
-		myTmpStore.setPrice(199.00f);
-		myTmpStore.setManufacturer("Amazon");
-		myTmpStore
-				.setDescription("17 cm (7 Zoll), Dualband -WLAN über zwei Antennen, "
-						+ "HD-Display, Dolby-Audio-Technologie, 16 GB oder 32 GB");
-		products.put(tmpArtNr, myTmpStore);
-		tmpArtNr++;
+    private void addDefaultData() {
+        // add some users
+        // add some products
+        Integer tmpArtNr = 123456;
 
-		myTmpStore = new Product();
-		myTmpStore.setName("Dell Latitude ST T11");
-		myTmpStore.setId(tmpArtNr);
-		myTmpStore.setPrice(729.00f);
-		myTmpStore.setManufacturer("Dell");
-		myTmpStore
-				.setDescription("25,7 cm (10,1 Zoll) Tablet-PC (Intel Atom Z670, 1,5GHz, "
-						+ "2GB RAM, 32GB SSD, Intel 600, Win 7 Pro)");
-		products.put(tmpArtNr, myTmpStore);
-		tmpArtNr++;
+        Product myTmpStore = new Product();
+        myTmpStore.setName("Kindle Fire HD");
+        myTmpStore.setId(tmpArtNr);
+        myTmpStore.setPrice(199.00f);
+        myTmpStore.setManufacturer("Amazon");
+        myTmpStore
+                .setDescription("17 cm (7 Zoll), Dualband -WLAN über zwei Antennen, "
+                        + "HD-Display, Dolby-Audio-Technologie, 16 GB oder 32 GB");
+        products.put(tmpArtNr, myTmpStore);
+        tmpArtNr++;
 
-		myTmpStore = new Product();
-		myTmpStore.setName("JAGUAR XF SPORTBRAKE");
-		myTmpStore.setId(tmpArtNr);
-		myTmpStore.setPrice(48.550f);
-		myTmpStore.setManufacturer("JAGUAR");
-		myTmpStore
-				.setDescription("3.0 Liter V6 Diesel S 275 PS (202kW): Kraftstoffverbrauch: "
-						+ "innerorts 8,6 l/100km, außerorts 4,9 l/100km, kombiniert "
-						+ "6,2 l/100km.CO2-Emission: 163 g/km; Effizienzklasse B");
-		products.put(tmpArtNr, myTmpStore);
-		tmpArtNr++;
-	}
+        myTmpStore = new Product();
+        myTmpStore.setName("Dell Latitude ST T11");
+        myTmpStore.setId(tmpArtNr);
+        myTmpStore.setPrice(729.00f);
+        myTmpStore.setManufacturer("Dell");
+        myTmpStore
+                .setDescription("25,7 cm (10,1 Zoll) Tablet-PC (Intel Atom Z670, 1,5GHz, "
+                        + "2GB RAM, 32GB SSD, Intel 600, Win 7 Pro)");
+        products.put(tmpArtNr, myTmpStore);
+        tmpArtNr++;
 
-	private Storage() {
-		products = new HashMap<Integer, Product>();
-		users = new HashMap<Integer, User>();
+        myTmpStore = new Product();
+        myTmpStore.setName("JAGUAR XF SPORTBRAKE");
+        myTmpStore.setId(tmpArtNr);
+        myTmpStore.setPrice(48.550f);
+        myTmpStore.setManufacturer("JAGUAR");
+        myTmpStore
+                .setDescription("3.0 Liter V6 Diesel S 275 PS (202kW): Kraftstoffverbrauch: "
+                        + "innerorts 8,6 l/100km, außerorts 4,9 l/100km, kombiniert "
+                        + "6,2 l/100km.CO2-Emission: 163 g/km; Effizienzklasse B");
+        products.put(tmpArtNr, myTmpStore);
+        tmpArtNr++;
+    }
 
-		addDefaultData();
-	}
+    private Storage() {
+        products = new HashMap<Integer, Product>();
+        users = new HashMap<Integer, User>();
 
-	public static Storage getInstance() {
-		return instance;
-	}
+        addDefaultData();
+    }
+
+    public static Storage getInstance() {
+        return instance;
+    }
 }
