7be41cc77eb7343098644728869087fb472ac24e
delbertooo <delbertooo@webentwicklung-vm>
2012-10-26 17:24:44 +0200

    Inserted products in the right way

--- src/java/data/Storage.java
+++ src/java/data/Storage.java
@@ -7,7 +7,7 @@ package data;
 import java.util.*;
 
 /**
- * 
+ *
  * @author christianlinde
  */
 public class Storage {
@@ -21,44 +21,35 @@ public class Storage {
 
     private void addDefaultData() {
         //add some users
-        addUser(new User(true, "Admin", "password", "admin@example.com", "Admin Street", "Admin Town"));
+        addUser(new User(
+                true, 
+                "Admin", 
+                "password", 
+                "admin@example.com", 
+                "Admin Street", 
+                "Admin Town"));
 
         //add some products
-/*		Integer tmpArtNr = 123456;
-		
-		Product myTmpStore = new Product();
-		myTmpStore.setName("Kindle Fire HD");
-		myTmpStore.setId(tmpArtNr);
-		myTmpStore.setPrice(199.00f);
-		myTmpStore.setManufacturer("Amazon");
-		myTmpStore
-				.setDescription("17 cm (7 Zoll), Dualband -WLAN über zwei Antennen, "
-						+ "HD-Display, Dolby-Audio-Technologie, 16 GB oder 32 GB");
-		products.put(tmpArtNr, myTmpStore);
-		tmpArtNr++;
+        addProduct(new Product(
+                "Kindle Fire HD",
+                199f,
+                "17 cm (7 Zoll), Dualband -WLAN über zwei Antennen, "
+                + "HD-Display, Dolby-Audio-Technologie, 16 GB oder 32 GB",
+                "Amazon"));
+        addProduct(new Product(
+                "Dell Latitude ST T11",
+                729.95f,
+                "25,7 cm (10,1 Zoll) Tablet-PC (Intel Atom Z670, 1,5GHz, "
+                + "2GB RAM, 32GB SSD, Intel 600, Win 7 Pro)",
+                "Dell"));
+        addProduct(new Product(
+                "JAGUAR XF SPORTBRAKE",
+                48550f,
+                "3.0 Liter V6 Diesel S 275 PS (202kW): Kraftstoffverbrauch: "
+                + "innerorts 8,6 l/100km, außerorts 4,9 l/100km, kombiniert "
+                + "6,2 l/100km.CO2-Emission: 163 g/km; Effizienzklasse B",
+                "JAGUAR"));
 
-		myTmpStore = new Product();
-		myTmpStore.setName("Dell Latitude ST T11");
-		myTmpStore.setId(tmpArtNr);
-		myTmpStore.setPrice(729.00f);
-		myTmpStore.setManufacturer("Dell");
-		myTmpStore
-				.setDescription("25,7 cm (10,1 Zoll) Tablet-PC (Intel Atom Z670, 1,5GHz, "
-						+ "2GB RAM, 32GB SSD, Intel 600, Win 7 Pro)");
-		products.put(tmpArtNr, myTmpStore);
-		tmpArtNr++;
-
-		myTmpStore = new Product();
-		myTmpStore.setName("JAGUAR XF SPORTBRAKE");
-		myTmpStore.setId(tmpArtNr);
-		myTmpStore.setPrice(48.550f);
-		myTmpStore.setManufacturer("JAGUAR");
-		myTmpStore
-				.setDescription("3.0 Liter V6 Diesel S 275 PS (202kW): Kraftstoffverbrauch: "
-						+ "innerorts 8,6 l/100km, außerorts 4,9 l/100km, kombiniert "
-						+ "6,2 l/100km.CO2-Emission: 163 g/km; Effizienzklasse B");
-		products.put(tmpArtNr, myTmpStore);
-		tmpArtNr++;*/
     }
 
     public void addUser(User u) {
@@ -92,7 +83,7 @@ public class Storage {
         }
         return res;
     }
-    
+
     public void addProduct(Product p) {
         p.setId(++autoIncProducts);
 
