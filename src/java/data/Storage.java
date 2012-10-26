23d6c47fd2d1393cf78b68cce0badd7dfbb78538
Robert Worgul <delbertooo@webentwicklung-vm.(none)>
2012-10-26 12:58:45 +0200

    Made Storage thread-safe singleton and added constructor

--- src/java/data/Storage.java
+++ src/java/data/Storage.java
@@ -4,6 +4,7 @@
  */
 package data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,9 +13,25 @@ import java.util.List;
  */
 public class Storage {
     
+    /* should be thread safe too: http://www.theserverside.de/singleton-pattern-in-java/ */
+    private static Storage instance = new Storage();
+    
     private List<Product> products;
     private List<User> users;
+    
+    private void addDefaultData() {
+        //users.add(...);
+        //products.add(...);
+    }
 
-    public Storage() {
+    private Storage() {
+        products = new ArrayList<Product>();
+        users = new ArrayList<User>();
+        
+        addDefaultData();
+    }
+    
+    public static Storage getInstance() {
+        return instance;
     }
 }
